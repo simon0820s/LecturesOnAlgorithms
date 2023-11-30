7db4dc6613c220f5ae138dfea410cb838b714941
simon0820s <simon0820s@gmail.com>
2023-11-30 16:14:12 -0500

    implement merge sort an make vs

--- sort/src/algorithms.rs
+++ sort/src/algorithms.rs
@@ -1,13 +1,55 @@
-pub fn insertion (mut list: Vec<f32>) -> Vec<f32>{
+pub fn insertion(mut arr: Vec<f32>) -> Vec<f32> {
+    for i in 1..arr.len() {
+        let mut j = i;
+        //Insert list[i] into sorted_list
+        while j > 0 && arr[j] < arr[j - 1] {
+            arr.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+    arr
+}
 
-  for i in 1..list.len() {
-      let mut j = i;
-      //Insert list[i] into sorted_list
-      while j > 0 && list[j] < list[j-1] {
-        list.swap(j, j-1);
-        j -= 1;
-      }
-    println!("{}", i)
-  }
-  list
+pub fn merge_sort(arr: &Vec<f32>) -> Vec<f32> {
+    if arr.len() < 2 {
+        arr.to_vec()
+    } else {
+        let size = arr.len() / 2;
+        let left_arr: Vec<f32> = merge_sort(&arr[0..size].to_vec());
+        let right_arr: Vec<f32> = merge_sort(&arr[size..].to_vec());
+        let merged: Vec<f32> = merge(&left_arr, &right_arr);
+        merged
+    }
+}
+
+fn merge(left: &Vec<f32>, right: &Vec<f32>) -> Vec<f32> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut merged: Vec<f32> = Vec::new();
+
+    while i < left.len() && j < right.len() {
+        if left[i] < right[j] {
+            merged.push(left[i]);
+            i += 1;
+        } else {
+            merged.push(right[j]);
+            j += 1;
+        }
+    }
+
+    if i < left.len() {
+        while i < left.len() {
+            merged.push(left[i]);
+            i += 1;
+        }
+    }
+
+    if j < right.len() {
+        while j < right.len() {
+            merged.push(right[j]);
+            j = j + 1;
+        }
+    }
+
+    merged
 }
--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,20 +1,29 @@
 mod algorithms;
 use algorithms::insertion;
-use std::time::Instant;
+use algorithms::merge_sort;
 use rand::prelude::*;
+use std::time::Instant;
+
 fn main() {
     //Create random list
-    let n: u64 = 1000000;
+    let n: u64 = 100000;
 
     let mut rng = rand::thread_rng();
 
     let list: Vec<f32> = (0..n).map(|_| rng.gen_range(0.0..100.0)).collect();
 
     let start_time = Instant::now();
-    let result = insertion(list);
+    let result = merge_sort(&list);
     let elapsed_time = start_time.elapsed();
 
     println!();
-    println!("result =>{:?}", result);
-    println!("required time => {}", elapsed_time.as_millis());
+    println!("Merge sort =>{:?}", &result[..10]);
+    println!("required time Merge sort => {}", elapsed_time.as_millis());
+
+    let start_time: Instant = Instant::now();
+    let result: Vec<f32> = insertion(list);
+    let elapsed_time = start_time.elapsed();
+    println!();
+    println!("Insertion sort =>{:?}", &result[..10]);
+    println!("required time Insertion sort => {}", elapsed_time.as_millis());
 }
