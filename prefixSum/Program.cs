546a6472240c03753e5fd13de19511c8e92b257b
simon0820s <simon0820s@gmail.com>
2023-12-12 21:53:16 -0500

    create prefix sum algorithm with C#

---
+++ prefixSum/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+class Program
+{
+
+  public static float[] prefixSum(float[] arr)
+  {
+
+    int n = arr.Length;
+    float[] prefixSum = new float[n];
+
+    prefixSum[0] = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+      prefixSum[i] = prefixSum[i - 1] + arr[i];
+    }
+
+    return prefixSum;
+  }
+  static void Main()
+  {
+    float[] arr = { 10.0f, 15.2f, 15.4f, 13.8f };
+    float[] prefixSumResult = prefixSum(arr);
+    Console.WriteLine(string.Join(", ", prefixSumResult));
+  }
+}
