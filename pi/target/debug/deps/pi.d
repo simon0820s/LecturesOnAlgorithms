8281d08a8c0a8d09a6ab1a1a28920ec6c133e39a
simon0820s <simon0820s@hotmail.com>
2024-02-16 06:25:47 -0500

    chore(pi): create project

---
+++ pi/target/debug/deps/pi.d
@@ -0,0 +1,5 @@
+C:\Users\simon\Desktop\programacion\algorithms\pi\target\debug\deps\pi.exe: src\main.rs
+
+C:\Users\simon\Desktop\programacion\algorithms\pi\target\debug\deps\pi.d: src\main.rs
+
+src\main.rs:
---
+++ pi/target/debug/pi.d
@@ -0,0 +1,1 @@
+C:\Users\simon\Desktop\programacion\algorithms\pi\target\debug\pi.exe: C:\Users\simon\Desktop\programacion\algorithms\pi\src\main.rs
